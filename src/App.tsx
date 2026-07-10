34794a2d061a79c946ced7d406eb112882aaba3e
Mayank <w1mayankz@gmail.com>
2026-07-10 13:33:29 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Hero } from './components/sections/Hero';
+import { About } from './components/sections/About';
+import { CaseStudies } from './components/sections/CaseStudies';
+
+function App() {
+  return (
+    <main className="w-full min-h-screen bg-[#EFEFEF] font-sans selection:bg-[#F26522] selection:text-white">
+      <Hero />
+      <About />
+      <CaseStudies />
+    </main>
+  );
+}
+
+export default App;

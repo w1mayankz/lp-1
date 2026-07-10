59aea608c0ff8b42805020f1ac34b1c242284052
Mayank <w1mayankz@gmail.com>
2026-07-10 13:51:54 +0530

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Menu, X, Clock, ArrowRight } from 'lucide-react';
 import { useLondonTime } from '../../hooks/useLondonTime';
 import { HoverButton } from '../ui/HoverButton';
